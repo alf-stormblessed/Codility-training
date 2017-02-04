9460b31f06dd363db411302115fb2ffc3641a856
Alfonso Casado <alficat@gmail.com>
2017-02-04 13:18:40 +0000

    pep8 compliant

--- Codility-training/src/codility/OddOccurrencesInArray/OddOccurrencesInArray.py
+++ Codility-training/src/codility/OddOccurrencesInArray/OddOccurrencesInArray.py
@@ -3,20 +3,23 @@ Created on 3 feb. 2017
 
 @author: accl
 
-Problem explanation: https://codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+Problem explanation:
+    https://codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
 
 @var len(A):  is an odd integer within the range [1..1,000,000];
 
-@var A: each element of array A is an integer within the range [1..1,000,000,000];
-        all but one of the values in A occur an even number of times.
+@var A: each element of array A is an integer
+    within the range [1..1,000,000,000];
+    all but one of the values in A occur an even number of times.
 
 '''
 
+
 def solution(A):
-    # write your code in Python 2.7
-    sum = 0
-    
+
+    total = 0
+
     for num in A:
-        sum ^= num
-        
-    return sum
+        total ^= num
+
+    return total
